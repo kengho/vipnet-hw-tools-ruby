3e9c1996b0cd9dc0b4f72845c126ef687d068277
Alexander Morozov <ntcomp12@gmail.com>
2016-04-06 17:59:15 -0400

    fixed errors when run script via cron

--- vipnet_coordinator_settings_grabber.rb
+++ vipnet_coordinator_settings_grabber.rb
@@ -34,6 +34,10 @@ module VipnetCoordinatorSettingsGrabber
         # end
         expect />\s*$/ do
           send "iplir show config"
+          # when running with cron, message appears:
+          # WARNING: terminal is not fully functional
+          #-  (press RETURN)
+          send 13 # enter
           logged = true
           puts "iplir show config"
         end
@@ -57,14 +61,25 @@ module VipnetCoordinatorSettingsGrabber
         end
       end
     end
+
     # remove regular trash caused by "send 34"
     iplirconf = iplirconf.gsub(":\e[K\r\e[K:\e[K3\b3\e[K4\b4\r\e[K", "")
     # remove trash in the beginning
     iplirconf = iplirconf.gsub("iplir show config\r\n\e[?1049h\e[?1h\e=\r", "")
     # remove trash in the end
-    iplirconf = iplirconf[/.*default= auto\r\n\r\n/m, 0]
+    iplirconf = iplirconf[/.*default= auto\r\n/m, 0]
     # replace \r\n by \n
     iplirconf = iplirconf.gsub("\r", "")
+    # dunno what's the source of this
+    iplirconf = iplirconf.gsub(":\x1B[K\x1B[K:\x1B[K1\x081\x1B[K3\x083\x1B[K", "")
+    # remove trash caused by "terminal is not fully functional" issue when running script with cron
+    iplirconf = iplirconf.gsub("::3\x083", "")
+    iplirconf = iplirconf.gsub("4\x084", "")
+    iplirconf = iplirconf.gsub("iplir show config\nWARNING: terminal is not fully functional\n-  (press RETURN):1\x081", "")
+    iplirconf = iplirconf.gsub("4\x084", "")
+    # add return if necessary
+    iplirconf += "\n" if iplirconf[-4..-1] != "\n\n"
+
     require "tempfile"
     iplirconf_file = Tempfile.new("iplirconf")
     iplirconf_file.write(iplirconf)
